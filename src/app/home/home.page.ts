9485428cec946233e435768fbb1891f853d5e903
Igor Rautiainen <igor.rautiainen@outlook.com>
2022-10-27 20:06:36 +0300

    Avatar upload live and running

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { AlertController, LoadingController } from '@ionic/angular';
 import { AuthService } from '../services/auth.service';
+import { AvatarService } from '../services/avatar.service';
 
 @Component({
   selector: 'app-home',
@@ -10,10 +13,43 @@ import { AuthService } from '../services/auth.service';
 export class HomePage {
   profile = null;
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private avatarService: AvatarService,
+    private authService: AuthService,
+    private router: Router,
+    private loadingController: LoadingController,
+    private alertController: AlertController
+
+  ) {
+    this.avatarService.getUserProfile().subscribe((data) => {
+      this.profile = data;
+    });
+  }
 
   async logout() {
     await this.authService.logout();
     this.router.navigateByUrl('/', { replaceUrl: true });
+  }
+
+  async changeImage() {
+    const image = await Camera.getPhoto({quality:90, allowEditing: false, resultType: CameraResultType.Base64, source: CameraSource.Photos,});
+
+    if (image) {
+      console.log(image);
+      const loading = await this.loadingController.create();
+      await loading.present();
+      const result = await this.avatarService.uploadImage(image); //returns null...
+
+      loading.dismiss();
+
+      if (!result) {{
+        const alert = await this.alertController.create({
+          header: 'Upload failed', 
+          message: 'Some problem happened', 
+          buttons: ['OK'],
+        });
+        await alert.present();
+      }}
+    }
   }
 }
