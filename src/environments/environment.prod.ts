a2a7d05df13e7dfd74dcf2d5d21ca9a10a4caaa7
Igor Rautiainen <igor.rautiainen@outlook.com>
2022-10-27 11:34:33 +0300

    @angular/fire added and connected

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'lab4-bc4de',
+    appId: '1:356212808918:web:16568fb2bbd94e732dfd77',
+    storageBucket: 'lab4-bc4de.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyCrD6bI9TyprYnH3YYaDRG2jYC83ON8qnY',
+    authDomain: 'lab4-bc4de.firebaseapp.com',
+    messagingSenderId: '356212808918',
+  },
   production: true
 };
